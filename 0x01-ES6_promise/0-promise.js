72f04c6c115aef1c3cd04c8a1c58a42dcd03c104
oyindamolajames <oyindamolajames@gmail.com>
2024-03-06 16:40:36 +0000

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,11 @@
 export default function getResponseFromAPI() {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     resolve(true);
-  })
+
+    const error = false;
+
+    if (!error) {
+      reject(new Error('Error: Something went wrong'));
+    }
+  });
 }
