bbdce2e9c39320c089131402ef22d118f297f37c
oyindamolajames <oyindamolajames@gmail.com>
2024-03-15 11:58:47 +0000

    task 3

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,4 @@
+import { RowElement, RowID } from './interface';
+export function insertRow(row: RowElement): RowID;
+export function updateRow(rowID: RowID, row: RowElement): RowID;
+export function deleteRow(rowID: RowID): void;
